696e09ca8c8884a287a68379358d514c1ab46572
zhanggh <849080458@qq.com>
2020-11-13 10:07:05 +0800

    新增pipenv命令

--- class1_env/make_env.py
+++ class1_env/make_env.py
@@ -37,4 +37,37 @@ pipenv install
 
 查看当前环境下安装包
 pipenv graph
+
+pipenv具有下列的选项：
+
+$ pipenv
+Usage: pipenv [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --where          显示项目文件所在路径
+  --venv           显示虚拟环境实际文件所在路径
+  --py             显示虚拟环境Python解释器所在路径
+  --envs           显示虚拟环境的选项变量
+  --rm             删除虚拟环境
+  --bare           最小化输出
+  --completion     完整输出
+  --man            显示帮助页面
+  --three / --two  使用Python 3/2创建虚拟环境（注意本机已安装的Python版本）
+  --python TEXT    指定某个Python版本作为虚拟环境的安装源
+  --site-packages  附带安装原Python解释器中的第三方库
+  --jumbotron      An easter egg, effectively.
+  --version        版本信息
+  -h, --help       帮助信息
+pipenv可使用的命令参数：
+
+Commands:
+  check      检查安全漏洞
+  graph      显示当前依赖关系图信息
+  install    安装虚拟环境或者第三方库
+  lock       锁定并生成Pipfile.lock文件
+  open       在编辑器中查看一个库
+  run        在虚拟环境中运行命令
+  shell      进入虚拟环境
+  uninstall  卸载一个库
+  update     卸载当前所有的包，并安装它们的最新版本
 """
