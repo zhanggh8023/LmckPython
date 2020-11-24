ce7079480457f760e792fc849b5abbef29f251e4
zhanggh <849080458@qq.com>
2020-11-24 20:47:27 +0800

    递归函数学习内容

---
+++ class5_recursion/recursion递归函数.py
@@ -0,0 +1,17 @@
+# ————————————————
+# @Time    : 2020/11/24 20:45
+# @Author  : zgh
+# @Email   : 849080458@qq.com
+# @File    : recursion递归函数.py
+# ————————————————
+
+# 递归函数
+
+
+# 死循环
+def func ():
+    print('999999')
+    func()
+
+
+func()
