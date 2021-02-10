dae2fc27f32df5dd5b6bbbc330dc8ba4daf5dd37
zhanggh <849080458@qq.com>
2021-02-10 13:32:01 +0800

    》完成第十七天的课程学习

--- class6_object_oriented/单例模式.py
+++ class6_object_oriented/单例模式.py
@@ -13,6 +13,7 @@
 # 2、在new方法中对类属性做判断：2.1没有创建就创建保存，并修改类属性值；2.2创建过就返回之前的
 '''
 
+
 # from class5_recursion.装饰器使用 import MyTest
 # t1=MyTest()
 # class MyClass(object):
@@ -30,22 +31,22 @@
 # 单例模式
 
 class MyTest(object):
-    isstane = None  # 设置一个类属性用来记录这个类有没有创建过对象
+    __instance = None  # 设置一个类属性用来记录这个类有没有创建过对象
 
-    def __new__(cls, *args, **kwargs):
-        if not cls.isstane:   # cls.isstane=None》False  not False 》true
+    def __new__ (cls, *args, **kwargs):
+        if not cls.__instance:  # cls.__instance=None》False  not False 》true
             # 如果类属性__instance的值为None，那么就创建一个对象
-            cls.isstane = object.__new__(cls)
-            return cls.isstane
+            cls.__instance = object.__new__(cls)
+            return cls.__instance
         else:
-            return cls.isstane
+            return cls.__instance
 
 # 装饰器实现单例模式
 
 
 # t1 = MyTest()
 # t1.name='zgh'
-#
+# MyTest.instance = None
 # t2=MyTest()
 # print(t2.name)
 # t3=MyTest()
@@ -53,36 +54,3 @@ class MyTest(object):
 # print(id(t1))
 # print(id(t2))
 # print(id(t3))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- class7_polymorphic/数据和自省.py
+++ class7_polymorphic/数据和自省.py
@@ -13,3 +13,22 @@ class Test:
     # 私有属性
     _attr2 = 2000
     __attr3 = 3000
+
+
+c = Test()
+
+# 类属性可以通过类和实例对象去访问
+
+print(Test.attr1)
+print(c.attr1)
+
+# 单下划线先开头的私有属性
+
+print(Test._attr2)
+print(c._attr2)
+
+# 双下划线开通的私有属性
+# 双下划线开头的私有熟悉对外不能直接访问，微辣保护这个变量对外改了一个名字。
+# 在原有的属性名前面加了一个_方法名
+print(Test._Test__attr3)
+print(Test.__dict__)
